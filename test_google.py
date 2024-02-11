7ebb28cc1de164de06c01e8b96e1e0b577c17be2
Zaur Mamedov <zaurmd1996@gmail.com>
2024-02-11 14:54:17 +0300

    Added negative test "test_find_nothing"

--- test_google.py
+++ test_google.py
@@ -4,10 +4,15 @@ from selene import browser, be, have
 
 @pytest.fixture(scope="function")
 def open_browser(setting_browser):
-    browser.open('https://google.com') # Open browser
+    browser.open('https://google.com')  # Open browser
     print('Browser is opened!')
 
 
-def test_should_find_selene(open_browser):
+def test_find_success(open_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_find_nothing(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('d\gfbnjg3').press_enter()
+    browser.element('[id="botstuff"]').should(have.text('ничего не найдено'))
