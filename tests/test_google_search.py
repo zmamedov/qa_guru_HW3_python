1753a36d8fc8fe308f0124d80a53a291216592d1
Zaur Mamedov <zaurmd1996@gmail.com>
2024-08-11 14:19:13 +0300

    Rename test

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,7 +1,7 @@
 from selene import browser, be, have
 
 
-def test_find_success(setting_browser):
+def test_success(setting_browser):
     browser.open('/')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
@@ -9,5 +9,5 @@ def test_find_success(setting_browser):
 
 def test_find_nothing(setting_browser):
     browser.open('/')
-    browser.element('[name="q"]').should(be.blank).type('d\gfbnjg3').press_enter()
+    browser.element('[name="q"]').should(be.blank).type('d*gfbnjg3').press_enter()
     browser.element('[id="botstuff"]').should(have.text('ничего не найдено'))
