dafd0de8c372658c22491ddaa85f9a5753d9b428
Zaur Mamedov <zaurmd1996@gmail.com>
2024-08-11 14:26:28 +0300

    Small fix in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,5 +7,7 @@ def setting_browser():
     browser.config.base_url = 'https://google.com'
     browser.config.window_height = 1080
     browser.config.window_width = 1920
+
     yield
+
     browser.quit()
